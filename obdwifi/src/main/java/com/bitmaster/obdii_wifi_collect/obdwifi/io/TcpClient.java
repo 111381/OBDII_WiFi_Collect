3c02a7f8a97ef5e9524b93b23db5298c8e070dcd
renet <renet@pro.ee>
2014-03-13 10:34:22 +0200

    Initial commit

---
+++ obdwifi/src/main/java/com/bitmaster/obdii_wifi_collect/obdwifi/io/TcpClient.java
@@ -0,0 +1,46 @@
+package com.bitmaster.obdii_wifi_collect.obdwifi.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.bitmaster.obdii_wifi_collect.obdwifi.MainActivity;
+
+/**
+ * Created by renet on 3/12/14.
+ */
+public class TcpClient {
+
+    public void runTcpClient() {
+        try {
+            InetAddress ip = InetAddress.getByName(MainActivity.SERVER_IP_ADDRESS);
+            Socket s = new Socket(ip, MainActivity.TCP_SERVER_PORT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+            //send output msg
+            String outMsg = "TCP connecting to " + MainActivity.TCP_SERVER_PORT + System.getProperty("line.separator");
+            out.write(outMsg);
+            out.flush();
+            Log.i("TcpClient", "sent: " + outMsg);
+            //accept server response
+            String inMsg = in.readLine() + System.getProperty("line.separator");
+            Log.i("TcpClient", "received: " + inMsg);
+            //close connection
+            s.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
