4fef93ea9c0b7e2ebcae54488442d678e582d5e0
renet <renet@pro.ee>
2014-04-01 11:14:37 +0300

    new branch

--- obdwifi/src/main/java/com/bitmaster/obdii_wifi_collect/obdwifi/io/TcpIntentService.java
+++ obdwifi/src/main/java/com/bitmaster/obdii_wifi_collect/obdwifi/io/TcpIntentService.java
@@ -2,12 +2,26 @@ package com.bitmaster.obdii_wifi_collect.obdwifi.io;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 /**
  * Created by uhh on 3/31/14.
  */
 public class TcpIntentService extends IntentService {
 
+    public static final int TCP_SERVER_PORT = 35000;
+    public static final String SERVER_IP_ADDRESS = "192.168.0.10";
+    public static final int SOCKET_CONN_TIMEOUT = 5000;
+
+    private Socket socket = null;
     /**
      * A constructor is required, and must call the super IntentService(String)
      * constructor with a name for the worker thread.
@@ -33,6 +47,35 @@ public class TcpIntentService extends IntentService {
                 } catch (Exception e) {
                 }
             }
+        }
+        String outMsg = "";
+        try {
+            InetAddress ip = InetAddress.getByName(SERVER_IP_ADDRESS);
+            this.socket = new Socket();
+            this.socket.connect(new InetSocketAddress(ip, TCP_SERVER_PORT), SOCKET_CONN_TIMEOUT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            //send output msg
+            out.write(outMsg + "\r");
+            out.flush();
+            Log.i("TcpClient", "sent: " + outMsg);
+            //accept server response
+            int character = 0;
+            String inMsg = "";
+            int bytes = 0; // if end character not found  && if task is not cancelled
+            while(((character = in.read()) != 62) && (bytes < 30)) { // EOL == '>'
+                inMsg = inMsg + Character.toString((char) character);
+                bytes++;
+            }
+            Log.i("TcpClient", "received: " + inMsg);
+            socket.close();
+
+            return ;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return ;
         }
     }
 }
